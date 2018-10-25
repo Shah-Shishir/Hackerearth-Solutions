68d194d7b41127a3ac60487f00d0c1593b9832a6
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-10-25 12:34:20 +0600

    Add files via upload

---
+++ N - Co Ordinates (Map Practice).cpp
@@ -0,0 +1,63 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define pii pair <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    int n,a,b,i=-1;
+    map <pair <int,int>,int> mp;
+    map <pair <int,int>,int> :: iterator it;
+
+    sf ("%d",&n);
+
+    while (n--)
+    {
+        sf ("%d %d",&a,&b);
+        mp [make_pair (a,b)]++;
+    }
+
+    for (it=mp.begin(); it!=mp.end(); ++it)
+        pf ("%d %d %d\n",it->first.first,it->first.second,it->second);
+
+    return 0;
+}
