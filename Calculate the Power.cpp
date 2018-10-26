2ab7f4ca91bcd847db531ebc58c049e46fa20151
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-10-27 04:54:45 +0600

    Add files via upload

---
+++ Calculate the Power.cpp
@@ -0,0 +1,83 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ebar_khela_hoppe int main (void)
+#define bair_ho return 0
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define vi vector <int>
+#define pii pair <int,int>
+#define mii map <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+bool CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+lld bigmod (lld base, lld power, lld mod)
+{
+    lld p1,p2;
+
+    if (power == 0)
+        return 1;
+    else if (power & 1)
+    {
+        p1 = base % mod;
+        p2 = (bigmod (base, power-1, mod)) % mod;
+
+        return (p1 * p2) % mod;
+    }
+    else if (power % 2 == 0)
+    {
+        p1 = (bigmod (base, power/2, mod)) % mod;
+
+        return (p1 * p1) % mod;
+    }
+}
+
+ebar_khela_hoppe
+{
+    /*
+    	freopen ("input.txt","r",stdin);
+    	freopen ("output.txt","w",stdout);
+    */
+
+    lld a,b;
+
+    sf ("%lld %lld",&a,&b);
+    pf ("%lld\n",bigmod(a,b,1000000007));
+
+    bair_ho;
+}
